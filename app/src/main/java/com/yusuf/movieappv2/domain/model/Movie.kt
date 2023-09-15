a2b6c32b42b333807a8d0f85883567881e9ded11
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-09-15 16:42:21 +0300

    Movie Model

---
+++ app/src/main/java/com/yusuf/movieappv2/domain/model/Movie.kt
@@ -0,0 +1,8 @@
+package com.yusuf.movieappv2.domain.model
+
+data class Movie (val id: Int,
+             val title: String,
+             val description: String,
+             val imageUrl: String,
+             val releaseDate: String)
+
