71813f53823a70607dc2db5db8d671d40fc89ec9
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-09-17 20:44:10 +0300

    popularMoviesDto.toMovieDetail Added

--- app/src/main/java/com/yusuf/movieappv2/data/util/Mappers.kt
+++ app/src/main/java/com/yusuf/movieappv2/data/util/Mappers.kt
@@ -1,6 +1,7 @@
 package com.yusuf.movieappv2.data.util
 
 import com.yusuf.movieappv2.data.remote.dto.movieDto.MovieDto
+import com.yusuf.movieappv2.data.remote.dto.popularMoviesDto.PopularMoviesDto
 import com.yusuf.movieappv2.domain.model.Movie
 
 internal fun MovieDto.toMovie(): Movie
@@ -12,6 +13,13 @@ internal fun MovieDto.toMovie(): Movie
         title = title,
         releaseDate = release_date
     )
+}
+
+
+fun PopularMoviesDto.toMovieDetail (): List<Movie>{
+    return results.map{
+            result ->  Movie(result.id,result.title,result.overview,result.poster_path,result.release_date)
+    }
 }
 
 private fun getImageUrl(imageUrl: String) = "https://image.tmdb.org/t/p/w500/$imageUrl"
