081db59532fe33fc4bad7ef496ac6995441b008e
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-09-17 20:45:14 +0300

    Movie Detail Use Case Added

---
+++ app/src/main/java/com/yusuf/movieappv2/domain/useCase/GetMovieDetailUseCase.kt
@@ -0,0 +1,17 @@
+package com.yusuf.movieappv2.domain.useCase
+
+import com.yusuf.movieappv2.domain.model.Movie
+import com.yusuf.movieappv2.domain.repository.MovieRepository
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class GetMovieDetailUseCase : KoinComponent{
+
+    private val repo: MovieRepository by inject()
+
+    @Throws(Exception::class)
+    suspend operator fun invoke(id:Int): Movie{
+       return repo.getMovieDetail(id=id)
+    }
+
+}
