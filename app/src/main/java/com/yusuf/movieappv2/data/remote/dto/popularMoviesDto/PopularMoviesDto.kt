f5dab66beedae177230f9666e037f4ac7eb709c9
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-09-17 20:43:47 +0300

    Update

--- app/src/main/java/com/yusuf/movieappv2/data/remote/dto/popularMoviesDto/PopularMoviesDto.kt
+++ app/src/main/java/com/yusuf/movieappv2/data/remote/dto/popularMoviesDto/PopularMoviesDto.kt
@@ -1,8 +1,13 @@
 package com.yusuf.movieappv2.data.remote.dto.popularMoviesDto
 
+import com.yusuf.movieappv2.domain.model.Movie
+
 data class PopularMoviesDto(
     val page: Int,
     val results: List<Result>,
     val total_pages: Int,
     val total_results: Int
 )
+
+
+
