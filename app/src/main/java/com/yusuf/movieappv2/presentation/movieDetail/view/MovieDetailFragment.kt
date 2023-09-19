7b3b9271de9475218b942433ec822a291aeeca85
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-09-19 21:27:45 +0300

    Fragments Added

---
+++ app/src/main/java/com/yusuf/movieappv2/presentation/movieDetail/view/MovieDetailFragment.kt
@@ -0,0 +1,29 @@
+package com.yusuf.movieappv2.presentation.movieDetail.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.yusuf.movieappv2.R
+
+class MovieDetailFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movie_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
