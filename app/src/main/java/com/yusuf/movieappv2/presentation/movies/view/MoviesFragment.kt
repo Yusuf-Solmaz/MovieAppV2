7a211f79141bcf4f09994246506ad1958bc7e0f2
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-09-21 19:12:53 +0300

    Fragment Completed

--- app/src/main/java/com/yusuf/movieappv2/presentation/movies/view/MoviesFragment.kt
+++ app/src/main/java/com/yusuf/movieappv2/presentation/movies/view/MoviesFragment.kt
@@ -5,10 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.yusuf.movieappv2.R
+import com.yusuf.movieappv2.databinding.FragmentMoviesBinding
+import com.yusuf.movieappv2.domain.model.Movie
+import com.yusuf.movieappv2.presentation.movies.MoviesViewModel
+import com.yusuf.movieappv2.presentation.movies.adapter.MoviesAdapter
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MoviesFragment : Fragment() {
+class MoviesFragment : Fragment(R.layout.fragment_movies),MoviesAdapter.Listener {
 
+    private var fragmentBinding: FragmentMoviesBinding? = null
+
+    private val viewModel by viewModel<MoviesViewModel>()
+    private lateinit var movieList: MutableList<Movie>
+
+    private lateinit var movieAdapter: MoviesAdapter
+    //    private var movieAdapter = MovieRecyclerViewAdapter(arrayListOf(),this)
+    private var job: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,11 +39,45 @@ class MoviesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_movies, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+       // viewModel = ViewModelProvider(requireActivity())[MoviesViewModel::class.java]
+
+        val binding = FragmentMoviesBinding.bind(view)
+        fragmentBinding = binding
+
+        val layoutManager = GridLayoutManager(requireContext(),2)
+        binding.recyclerView.layoutManager = layoutManager
+
+        movieList = mutableListOf()
+
+        movieAdapter = MoviesAdapter(movieList,this@MoviesFragment)
+        binding.recyclerView.adapter = movieAdapter
+
+        viewModel.getMovies()
+
+        val state = viewModel.state
+
+        lifecycleScope.launch {
+            state.collect(){
+                val newMovieList = it.movies
+
+                if (newMovieList.isNotEmpty()){
+
+                    movieList.clear()
+                    movieList.addAll(newMovieList)
+                    movieAdapter.updateData(newMovieList)
+                    println("Liste: $movieList")
+                }
+            }
+        }
+    }
+
+    override fun onItemClick(movieModel: Movie) {
+        TODO("Not yet implemented")
     }
 }
