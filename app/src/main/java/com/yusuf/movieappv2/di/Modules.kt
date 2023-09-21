758fd064492f6e98c4be331e5b4eb104a8b9770a
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-09-21 19:12:16 +0300

    viewModel module added

--- app/src/main/java/com/yusuf/movieappv2/di/Modules.kt
+++ app/src/main/java/com/yusuf/movieappv2/di/Modules.kt
@@ -5,10 +5,13 @@ import com.yusuf.movieappv2.data.repository.MovieRepositoryImp
 import com.yusuf.movieappv2.domain.repository.MovieRepository
 import com.yusuf.movieappv2.domain.useCase.GetMovieDetailUseCase
 import com.yusuf.movieappv2.domain.useCase.GetMovieUseCase
+import com.yusuf.movieappv2.presentation.movies.MoviesViewModel
 import com.yusuf.movieappv2.util.Constants.BASE_URL
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import org.koin.androidx.viewmodel.dsl.viewModel
+
 
 val appModule = module {
 
@@ -22,6 +25,10 @@ val appModule = module {
 
     single<MovieRepository> {
         MovieRepositoryImp(get())
+    }
+
+    viewModel {
+        MoviesViewModel(get())
     }
 
     factory { GetMovieDetailUseCase() }
