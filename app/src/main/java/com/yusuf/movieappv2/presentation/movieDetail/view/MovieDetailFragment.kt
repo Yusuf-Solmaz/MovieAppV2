e4f47cf12a5b85e83a155d44f5aaf68d1cd43453
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-09-22 17:22:19 +0300

    Movie Detail Fragment Completed

--- app/src/main/java/com/yusuf/movieappv2/presentation/movieDetail/view/MovieDetailFragment.kt
+++ app/src/main/java/com/yusuf/movieappv2/presentation/movieDetail/view/MovieDetailFragment.kt
@@ -5,10 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.yusuf.movieappv2.R
+import com.yusuf.movieappv2.data.util.getImageUrl
+import com.yusuf.movieappv2.databinding.FragmentMovieDetailBinding
+import com.yusuf.movieappv2.presentation.movieDetail.MovieDetailViewModel
+import kotlinx.coroutines.launch
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MovieDetailFragment : Fragment() {
 
+    private var fragmentBinding: FragmentMovieDetailBinding? = null
+    private val viewModel by viewModel<MovieDetailViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,11 +29,38 @@ class MovieDetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_movie_detail, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentMovieDetailBinding.bind(view)
+        fragmentBinding = binding
+
+        arguments?.let {
+            val movieId = MovieDetailFragmentArgs.fromBundle(it).movieId
+            viewModel.setImdbId(movieId= movieId)
+        }
+
+        val state = viewModel.state
+
+        lifecycleScope.launch {
+            state.collect{
+                val movie = it.movie
+                if (movie!=null){
+                    binding.movieName.text = movie.title
+                    binding.movieDetails.text = movie.description
+                    binding.movieReleaseDate.text = movie.releaseDate
+
+                    Glide.with(requireContext())
+                        .load(getImageUrl(movie.imageUrl))
+                        .placeholder(R.drawable.img)
+                        .transition(DrawableTransitionOptions.withCrossFade())
+                        .into(binding.recyclerViewImage)
+                }
+
+            }
+        }
     }
 }
