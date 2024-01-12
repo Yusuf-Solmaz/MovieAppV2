46a63b2b54e6da91b95ff3782ea771843cee875f
Yusuf Mücahit Solmaz <83172478+Yusuf-Solmaz@users.noreply.github.com>
2024-01-12 18:49:34 +0300

    Update Constants.kt

--- app/src/main/java/com/yusuf/movieappv2/util/Constants.kt
+++ app/src/main/java/com/yusuf/movieappv2/util/Constants.kt
@@ -2,6 +2,6 @@ package com.yusuf.movieappv2.util
 
 object Constants {
 
-    const val API_KEY = "77396af91815c9da24186f792a95e391"
+    const val API_KEY = "enter your api key here"
     const val BASE_URL = "https://api.themoviedb.org/"
 }
